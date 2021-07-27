9b763e8e7be94774c6bdbb418fafc32152caaa50
DarkKnight <xyz19940216@163.com>
2021-07-27 15:41:56 +0800

    Add existing ontologies for reuse

---
+++ src/import/Disease/mergetxt2owl.bat
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+type *.txt.>>DO.txt
+=======
+type *.txt.>>DO.txt
+>>>>>>> 32e8272b4d5b0468f0760ee02e47ac567794c291
+robot extract --method BOT --input HumanDO.owl --term-file DO.txt --output disease.owl
